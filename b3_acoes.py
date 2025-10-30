b3fb23e59c77811e9ce1ef672dd94a0a3c1bab3f
LFAJOGA5 <alvesfernandoluis16@gmail.com>
2025-10-29 23:12:08 -0300

    Adicao dos arquivos de requirements e script.py

---
+++ b3_acoes.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import yfinance as yf
+
+st.set_page_config(
+    page_title="PAINEL DE AÇÕES B3",
+    layout = "wide"
+)
+
+st.header("**PAINEL DE PREÇO DE FECHAMENTO E DIVIDENDOS DE AÇÕES DA B3**")
+
+ticker = st.text_input("Digite o ticker/sigla da ação", "PETR4")
+empresa = yf.Ticker(f"{ticker}.SA")
+
+tickerdf = empresa.history(start="2019-01-01", end="2025-09-30")
+
+
+col1, col2, col3 = st.columns([1,1,1])
+
+with col1:
+    st.write(f"**Empresa:**{empresa.info['longName']}")
+with col2:
+    st.write(f"**Mercado:** {empresa.info['industry']}")
+with col3:
+    st.write(f"**Preço atual:** {empresa.info['currentPrice']}")
+
+st.line_chart(tickerdf.Close)
+st.bar_chart(tickerdf.Dividends)
