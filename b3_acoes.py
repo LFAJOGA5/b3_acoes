30831345d746367adbfa9ac2de2404da03c16390
LFAJOGA5 <alvesfernandoluis16@gmail.com>
2025-10-29 23:26:49 -0300

    Add files via upload

--- b3_acoes.py
+++ b3_acoes.py
@@ -17,7 +17,7 @@ tickerdf = empresa.history(start="2019-01-01", end="2025-09-30")
 col1, col2, col3 = st.columns([1,1,1])
 
 with col1:
-    st.write(f"**Empresa:**{empresa.info['longName']}")
+    st.write(f"**Empresa:** {empresa.info['longName']}")
 with col2:
     st.write(f"**Mercado:** {empresa.info['industry']}")
 with col3:
